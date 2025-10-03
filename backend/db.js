86d5b2773c279d70ca7049a73be3edd589c85b92
Dskter <dskter.sahil@gmail.com>
2025-10-03 20:46:45 +0530

    Fixed

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const { Pool } = pkg;
 const pool = new Pool({
   user: 'avnadmin',
   host: 'pg-ef7f49a-dskterplays-fc75.e.aivencloud.com',
-  database: 'cinescopedb',
+  database: 'defaultdb',
   password: 'AVNS_tIQzL9oi3DAWd9A7Ua7',
   port: 13663,
   ssl: {
